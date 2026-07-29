92c68c3e0e9ab345c11cd9608a6b20fa47fe6920
Kette Pryce <johnsmusicbox@gmail.com>
2026-07-29 16:17:58 -0400

    fix(26505): [Good First Issue] 🍥 Add new Video Game Quote 15 - Beginner-

---
+++ solutions/bounty_fix_26505_1785356263.py
@@ -0,0 +1,107 @@
+[
+  {
+    "id": 1,
+    "quote": "War. War never changes.",
+    "character": "Narrator",
+    "game": "Fallout",
+    "year": 1997
+  },
+  {
+    "id": 2,
+    "quote": "It's dangerous to go alone! Take this.",
+    "character": "Old Man",
+    "game": "The Legend of Zelda",
+    "year": 1986
+  },
+  {
+    "id": 3,
+    "quote": "Would you kindly?",
+    "character": "Atlas / Andrew Ryan",
+    "game": "BioShock",
+    "year": 2007
+  },
+  {
+    "id": 4,
+    "quote": "The right man in the wrong place can make all the difference in the world.",
+    "character": "G-Man",
+    "game": "Half-Life 2",
+    "year": 2004
+  },
+  {
+    "id": 5,
+    "quote": "Praise the Sun!",
+    "character": "Solaire of Astora",
+    "game": "Dark Souls",
+    "year": 2011
+  },
+  {
+    "id": 6,
+    "quote": "Finish Him!",
+    "character": "Announcer",
+    "game": "Mortal Kombat",
+    "year": 1992
+  },
+  {
+    "id": 7,
+    "quote": "Do a a barrel roll!",
+    "character": "Peppy Hare",
+    "game": "Star Fox 64",
+    "year": 1997
+  },
+  {
+    "id": 8,
+    "quote": "Snake? Snake? Snaaaake!",
+    "character": "Roy Campbell",
+    "game": "Metal Gear Solid",
+    "year": 1998
+  },
+  {
+    "id": 9,
+    "quote": "The cake is a lie.",
+    "character": "Graffiti / Doug Rattmann",
+    "game": "Portal",
+    "year": 2007
+  },
+  {
+    "id": 10,
+    "quote": "Had to be me. Someone else might have gotten it wrong.",
+    "character": "Mordin Solus",
+    "game": "Mass Effect 3",
+    "year": 2012
+  },
+  {
+    "id": 11,
+    "quote": "Stay awhile and listen!",
+    "character": "Deckard Cain",
+    "game": "Diablo II",
+    "year": 2000
+  },
+  {
+    "id": 12,
+    "quote": "Requiescat in pace.",
+    "character": "Ezio Auditore da Firenze",
+    "game": "Assassin's Creed II",
+    "year": 2009
+  },
+  {
+    "id": 13,
+    "quote": "Keep your expectations low, boy, and you'll never be disappointed.",
+    "character": "Kratos",
+    "game": "God of War",
+    "year": 2018
+  },
+  {
+    "id": 14,
+    "quote": "Protocol 3: Protect the Pilot.",
+    "character": "BT-7274",
+    "game": "Titanfall 2",
+    "year": 2016
+  },
+  {
+    "id": 15,
+    "quote": "Stand amongst the ashes of a trillion dead souls, and ask the ghosts if honor matters. The silence is your answer.",
+    "character": "Javik",
+    "game": "Mass Effect 3",
+    "year": 2012
+  }
+]
